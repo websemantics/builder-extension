b2884520065dce28e860516c92beea66fbab5061
Adnan <adnan@websemantics.ca>
2016-02-19 11:00:29 -0500

    Improve field label, not convert from snake case to human redable

---
+++ src/Parser/AssignmentLabelParser.php
@@ -0,0 +1,33 @@
+<?php namespace Websemantics\EntityBuilderExtension\Parser;
+
+use Anomaly\Streams\Platform\Assignment\AssignmentModel;
+
+/**
+ * Class AssignmentLabelParser
+ *
+ * @link      http://websemantics.ca/ibuild
+ * @link      http://ibuild.io
+ * @author    WebSemantics, Inc. <info@websemantics.ca>
+ * @author    Adnan Sagar <msagar@websemantics.ca>
+ * @copyright 2012-2015 Web Semantics, Inc.
+ * @package   Websemantics\EntityBuilderExtension
+ */
+
+class AssignmentLabelParser
+{
+
+    /**
+     * Return the module name.
+     *
+     * @param  Module $module
+     * @return string
+     */
+    public function parse(AssignmentModel $assignment)
+    {
+        return ucwords(str_replace('_',' ', $assignment->getFieldSlug()));
+    }
+
+}
+
+
+

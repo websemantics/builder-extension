4a6216bf9be9d55446f7a07a0055501b717f6462
Adnan <adnan@websemantics.ca>
2016-08-14 22:03:03 -0400

    Adding class attributes placeholders for EntityModel

--- resources/assets/entity/code/{namespace}/{entity_name}/{entity_name}Model.php
+++ resources/assets/entity/code/{namespace}/{entity_name}/{entity_name}Model.php
@@ -12,6 +12,12 @@ use Anomaly\Streams\Platform\Model\{namespace}\{namespace}{studly_case_stream_sl
 
 class {entity_name}Model extends {namespace}{studly_case_stream_slug}EntryModel implements {entity_name}Interface
 {
+    /**
+     * Disable timestamps for this model.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 
     /**
      * The number of minutes to cache query results.
@@ -19,5 +25,20 @@ class {entity_name}Model extends {namespace}{studly_case_stream_slug}EntryModel 
      * @var null|false|int
      */
     protected $ttl = false;
+
+    /**
+     * The attributes that are
+     * not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Runtime cache.
+     *
+     * @var array
+     */
+    protected $cache = [];
 
 }

e0874eeb87ad4216427780f9554757143570e801
Adnan <adnan@websemantics.ca>
2016-08-14 21:53:18 -0400

    Adding class attributes placeholders to FormBuilder template

--- resources/assets/entity/code/{namespace}/{entity_name}/Form/{entity_name}FormBuilder.php
+++ resources/assets/entity/code/{namespace}/{entity_name}/Form/{entity_name}FormBuilder.php
@@ -18,4 +18,53 @@ class {entity_name}FormBuilder extends FormBuilder
      */
     protected $fields = [];
 
+    /**
+     * The form fields.
+     *
+     * @var array
+     */
+    protected $fields = [];
+
+    /**
+     * Fields to skip.
+     *
+     * @var array
+     */
+    protected $skips = [];
+
+    /**
+     * The form actions.
+     *
+     * @var array
+     */
+    protected $actions = [];
+
+    /**
+     * The form buttons.
+     *
+     * @var array
+     */
+    protected $buttons = [];
+
+    /**
+     * The form options.
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * The form sections.
+     *
+     * @var array
+     */
+    protected $sections = [];
+
+    /**
+     * The form assets.
+     *
+     * @var array
+     */
+    protected $assets = [];
+
 }

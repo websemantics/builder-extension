ee0529be090e780014e6d1701de13da2d2aa4141
Adnan <adnan@websemantics.ca>
2016-08-14 21:51:09 -0400

    Extending Entity contracts with Entry Anomaly Entry classes

--- resources/assets/entity/code/{namespace}/{entity_name}/Contract/{entity_name}Interface.php
+++ resources/assets/entity/code/{namespace}/{entity_name}/Contract/{entity_name}Interface.php
@@ -1,5 +1,7 @@
 <?php namespace {vendor_name}\{module_name}Module\{namespace_folder}{entity_name}\Contract;
 
+use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
+
 /**
  * Interface {entity_name}Interface
  *
@@ -7,7 +9,7 @@
  * @package   {vendor_name}\{module_name}Module\{namespace_folder}{entity_name}\Contract
  */
 
-interface {entity_name}Interface
+interface {entity_name}Interface extends EntryInterface
 {
 
 }

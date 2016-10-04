585b78af10cd9e81925b55a05d8f23864c8d805f
Adnan <adnan@websemantics.ca>
2016-10-03 23:12:15 -0400

    Adding a new route for an entity to cover module home in case there's not home :/

--- resources/stubs/entity/templates/module/routes.php
+++ resources/stubs/entity/templates/module/routes.php
@@ -1,4 +1,5 @@
 <?php
-['admin/{{module_slug|lower}}/{{entity_name|lower|str_plural}}'            => '{{vendor|studly_case}}\{{module_slug|studly_case}}Module\Http\Controller\Admin\{{entity_name|str_plural}}Controller@index',
+['admin/{{module_slug|lower}}'            => '{{vendor|studly_case}}\{{module_slug|studly_case}}Module\Http\Controller\Admin\{{entity_name|str_plural}}Controller@index',
+'admin/{{module_slug|lower}}/{{entity_name|lower|str_plural}}'            => '{{vendor|studly_case}}\{{module_slug|studly_case}}Module\Http\Controller\Admin\{{entity_name|str_plural}}Controller@index',
 'admin/{{module_slug|lower}}/{{entity_name|lower|str_plural}}/create'     => '{{vendor|studly_case}}\{{module_slug|studly_case}}Module\Http\Controller\Admin\{{entity_name|str_plural}}Controller@create',
 'admin/{{module_slug|lower}}/{{entity_name|lower|str_plural}}/edit/{id}'  => '{{vendor|studly_case}}\{{module_slug|studly_case}}Module\Http\Controller\Admin\{{entity_name|str_plural}}Controller@edit'];
